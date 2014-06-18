815ad66afbee3b4f22be74fc8c5dd8d3b50656b2
Fernando Crespo <fernando82@gmail.com>
2014-06-18 13:51:10 -0300

    Various changes in add/remove/resize lists

--- lastphotos.py
+++ lastphotos.py
@@ -2,18 +2,20 @@
     Make a list of the last x photos based and sorted by
     the filename
 """
+from __future__ import print_function
 # Begin customization -------------------------------------
 
-QUANTITY = 10
-SOURCE_DIR=u'~/test/photos'
-YEARS = xrange(2009, 2014) #exclude last
-DESTINATION_DIR=u'~/test/photos/2014/12 - Dezembro/'
+QUANTITY = 1000
+SOURCE_DIR=u'/volume1/photo'
+YEARS = xrange(2009, 2015) #exclude last
+DESTINATION_DIR=u'/volume1/backups/Dropbox/Fernando/Fotos dropbox Thais'
 
 # End customization ---------------------------------------
 # ---------------------------------------------------------
 import os
 from shutil import copy2
 from pprint import pprint
+from PIL import Image
 #__debug__ = True
 
 years_paths = [ os.path.realpath(os.path.expanduser(os.path.join(SOURCE_DIR, str(year)))) for year in YEARS ]
@@ -26,7 +28,9 @@ def get_last_photos(quantity):
     for year_path in years_paths:
         for root, dirs, files in os.walk(year_path):
             dirs.sort(reverse=True)
+            del(dirs[0])
             files.sort(reverse=True)
+            files = [ _file for _file in files if _file.endswith(u'.jpg') and not _file.endswith(u'.DS_Store')]
             already_added = len(_LAST_X_PHOTOS)
             if __debug__: print(root, len(files), len(_LAST_X_PHOTOS))
             if already_added < quantity:
@@ -35,17 +39,72 @@ def get_last_photos(quantity):
                 _LAST_X_PHOTOS = map(fullpath, _path, _LAST_X_PHOTOS)
             else:
                 break
+        if len(_LAST_X_PHOTOS) >= quantity:
+            break
     return _LAST_X_PHOTOS
 
+def resize(image):
+    print(u'r', end='')
+    img = Image.open(image)
+    if img.size[0]*img.size[1]>5000000:
+        new_img = img.resize((img.size[0]/2, img.size[1]/2))
+        new_img.save(image, exif=img.info[u'exif'])
+
+def bigger(image):
+    img = Image.open(image)
+    return True if img.size[0]*img.size[1]>5000000 else False
+
 if __name__ == '__main__':
-    photos_origin = get_last_photos(QUANTITY)
-    photos_destination = map(os.path.basename, photos_origin)
-    photos_destination = map(os.path.join, [os.path.expanduser(DESTINATION_DIR)]*len(photos_destination), photos_destination)
-    pprint(photos_destination)
+    last_photos = get_last_photos(QUANTITY)
 
-    _existing_photos = os.listdir(os.path.expanduser(DESTINATION_DIR))[:10]
-    existing_photos = map(os.path.join, [os.path.expanduser(DESTINATION_DIR)]*len(_existing_photos), _existing_photos)
-    pprint(existing_photos)
+    # print(last_photos)
+    only_names = [ os.path.basename(photo) for photo in last_photos ]
+    only_names = [ photo for photo in only_names if not photo.endswith(u'.DS_Store') ] # Fix photos selected
+    # print(only_names)
 
-    pprint(set(photos_destination) - set(existing_photos))
-    map(copy2, photos_origin, photos_destination)
+    photos_on_destination = os.listdir(os.path.expanduser(DESTINATION_DIR))
+    only_names_on_destination = [ os.path.basename(photo) for photo in photos_on_destination if not photo.endswith(u'.DS_Store') ] # Fix photos selected
+    # print(only_names_on_destination)
+
+    photos_to_add = set(only_names) - set(only_names_on_destination)
+    photos_to_remove = set(only_names_on_destination) - set(only_names)
+
+    photos_to_add_tuple_list = [ (photo, os.path.join(os.path.expanduser(DESTINATION_DIR), os.path.basename(photo))) for photo in last_photos if os.path.basename(photo) in photos_to_add ]
+    photos_to_remove_fullpath = [ os.path.join(os.path.expanduser(DESTINATION_DIR), photo) for photo in photos_to_remove ]
+
+    # print(u'add', photos_to_add_tuple_list)
+    # print(u'remove', photos_to_remove_fullpath)
+
+    total_photos_to_add = len(photos_to_add_tuple_list)
+    print(u'Adding {} photos'.format(total_photos_to_add))
+    for i, item in enumerate(photos_to_add_tuple_list):
+        src, dst = item
+        print(total_photos_to_add - i, end=u'.')
+        # print(src, dst)
+        # copy2(src, dst)
+    print()
+
+    total_photos_to_remove = len(photos_to_remove_fullpath)
+    print(u'Removing {} photos'.format(total_photos_to_remove))
+    for i, photo in enumerate(photos_to_remove_fullpath):
+        print(total_photos_to_remove - i, end=u'.')
+        # print(photo)
+        # os.remove(photo)
+    print()
+
+    photos_to_resize = os.listdir(os.path.expanduser(DESTINATION_DIR))
+    photos_to_resize = [ os.path.join(os.path.expanduser(DESTINATION_DIR), os.path.basename(photo)) for photo in photos_to_resize if not photo.endswith(u'.DS_Store') and bigger(os.path.join(os.path.expanduser(DESTINATION_DIR), os.path.basename(photo))) ]
+
+    # photos_to_resize = photos_to_resize[:10]
+    print(u'resize', photos_to_resize)
+
+    # total_photos_to_resize = len(photos_to_resize)
+    # print(u'Resizing {} photos'.format(total_photos_to_resize))
+    # for i, photo in enumerate(photos_to_resize):
+    #     print(total_photos_to_resize - i, end=u'.')
+    #     #print()
+    #     #print(photo)
+    #     img = Image.open(photo)
+    #     new_img = img.resize((img.size[0]/2, img.size[1]/2))
+    #     new_img.save(photo, exif=img.info[u'exif'])
+    # print()
